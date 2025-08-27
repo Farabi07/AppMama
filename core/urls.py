69c7fb5c81b726bab847409fc3ec53c1161e5a8e
farabi07 <farhadkabir1212@gmail.com>
2025-08-27 06:21:51 +0600

    keep all ai code in one file

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 # urls.py in the 'ocr' app
 from django.urls import path
-from .views import ReceiptUploadView, TaskMamaAPI
+from .views import ReceiptUploadView, handle_task_mama_request
 
 urlpatterns = [
     path('upload_receipt/', ReceiptUploadView.as_view(), name='upload_receipt'),
-    path('api/chat/', TaskMamaAPI.as_view(), name='task_mama_chat'),
+    # path('api/chat/', TaskMamaAPI.as_view(), name='task_mama_chat'),
+    # path('api/task-mama/', TaskMamaAPIView.as_view(), name='task_mama_api'),
+    path('api/task-mama/handle/', handle_task_mama_request, name='handle_task_mama_request'),
 ]
